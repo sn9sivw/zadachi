842bdf40d86c7e3323f16f2f49d7cb2b379d9115
sn9sivw <ilamokryakov@gmail.com>
2024-12-10 10:28:50 +0000

    Сделал

--- task40.py
+++ task40.py
@@ -1,3 +1,9 @@
-x = int(input("x: "))
-y = x * x * x + 2.5 * x * x - x + 1
-print("y: ", y)
+from math import sin
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+
+
+x = round(((2 / (((a ** 2) + 25) + b)) + b) / ((b ** 0.5) + ((a + b) / 2)), 2)
+y = round((abs(a) + (2 * sin(b))) / (5.5 * a), 2)
+print("x = ", x)
+print("y = ", y)
