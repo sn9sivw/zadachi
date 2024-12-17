a9b60ec3d1bff06aa6786a228893f07a95428ed4
sn9sivw <ilamokryakov@gmail.com>
2024-12-17 08:10:01 +0000

    Сделал

---
+++ task54.py
@@ -0,0 +1,8 @@
+x = int(input("Введите возраст Тани: "))
+y = int(input("Введите возраст Мити: "))
+sa = round((x + y) / 2, 0)
+b = x - sa
+c = y - sa
+print("Средний возраст:", sa)
+print(b)
+print(c)
